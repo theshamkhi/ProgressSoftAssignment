900e90eff20a416e4bd2af232fcdacd03f9575f0
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-11-27 10:53:31 +0100

    FEAT(dto): add DTO for Deal input/response mapping

--- src/main/java/com/progressoft/fxdeals/dto/DealDTO.java
+++ src/main/java/com/progressoft/fxdeals/dto/DealDTO.java
@@ -1,4 +1,36 @@
 package com.progressoft.fxdeals.dto;
 
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DealDTO {
+
+    @CsvBindByName(column = "dealId", required = true)
+    private String dealId;
+
+    @CsvBindByName(column = "fromCurrency", required = true)
+    private String fromCurrency;
+
+    @CsvBindByName(column = "toCurrency", required = true)
+    private String toCurrency;
+
+    @CsvBindByName(column = "dealTimestamp", required = true)
+    private String dealTimestamp;
+
+    @CsvBindByName(column = "dealAmount", required = true)
+    private String dealAmount;
+
+    private LocalDateTime parsedTimestamp;
+    private BigDecimal parsedAmount;
 }
