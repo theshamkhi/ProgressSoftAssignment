7b0658ff2ec2df7bd7fad69addf9a807b35c4768
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-11-27 10:58:18 +0100

    FEAT(repository): add JPA repository for Deal entity

--- src/main/java/com/progressoft/fxdeals/repository/DealRepository.java
+++ src/main/java/com/progressoft/fxdeals/repository/DealRepository.java
@@ -1,4 +1,11 @@
 package com.progressoft.fxdeals.repository;
 
-public class DealRepository {
+import com.progressoft.fxdeals.model.Deal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DealRepository extends JpaRepository<Deal, String> {
+
+    boolean existsByDealId(String dealId);
 }
