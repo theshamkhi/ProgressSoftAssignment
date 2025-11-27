a5b26121df0b0649bef5e504822e34b5c11371f2
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2025-11-27 10:54:58 +0100

    FEAT(dto): create result DTO to return import summary

--- src/main/java/com/progressoft/fxdeals/dto/ImportResultDTO.java
+++ src/main/java/com/progressoft/fxdeals/dto/ImportResultDTO.java
@@ -1,4 +1,29 @@
 package com.progressoft.fxdeals.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ImportResultDTO {
+
+    private int totalRows;
+    private int successfulInserts;
+    private int duplicates;
+    private int validationErrors;
+    private int databaseErrors;
+
+    @Builder.Default
+    private List<String> errorMessages = new ArrayList<>();
+
+    public void addErrorMessage(String message) {
+        errorMessages.add(message);
+    }
 }
